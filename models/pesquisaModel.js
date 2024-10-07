e17cbf5ce361990a65540a9dcd54f67b0993f9f3
Thur00 <arthursvinhedo@gmail.com>
2024-10-07 14:19:11 -0300

    correção pesquisa

--- models/pesquisaModel.js
+++ models/pesquisaModel.js
@@ -1,3 +1,57 @@
+// models/pesquisaModel.js
+
+// Importa o Request e os tipos de dados (TYPES) do pacote "tedious" para criar e executar consultas SQL
+const { Request, TYPES } = require("tedious");
+
+// Importa a função que conecta ao banco de dados
+const connectDatabase = require("../db/connection");
+
+// Função genérica para executar uma query SQL
+async function executeQuery(query, params = []) {
+  // Estabelece uma conexão com o banco de dados
+  const connection = await connectDatabase();
+
+  // Retorna uma Promise para lidar com a execução assíncrona da query
+  return new Promise((resolve, reject) => {
+    // Cria uma nova requisição SQL com a query passada e um callback para erros
+    const request = new Request(query, (err) => {
+      if (err) {
+        // Se ocorrer um erro, a Promise é rejeitada e a conexão é fechada
+        reject(err);
+        connection.close();
+      }
+    });
+
+    // Adiciona parâmetros à requisição SQL (nome, tipo e valor)
+    params.forEach(({ name, type, value }) => {
+      request.addParameter(name, type, value);
+    });
+
+    // Array para armazenar os resultados retornados pela query
+    let results = [];
+
+    // Evento "row" é disparado para cada linha retornada pela query
+    request.on("row", (columns) => {
+      // Cria um objeto para cada linha e armazena suas colunas e valores
+      let row = {};
+      columns.forEach((column) => {
+        row[column.metadata.colName] = column.value;
+      });
+      results.push(row);
+    });
+
+    // Evento "requestCompleted" é disparado quando a query é completamente executada
+    request.on("requestCompleted", () => {
+      // Fecha a conexão com o banco de dados e resolve a Promise com os resultados
+      connection.close();
+      resolve(results);
+    });
+
+    // Executa a requisição SQL
+    connection.execSql(request);
+  });
+}
+
 async function getAlunoByRm(rm) {
   const query = "SELECT * FROM Aluno WHERE RM = @rm;"; // Query SQL com um parâmetro para filtrar pelo ID
   const params = [{ name: "rm", type: TYPES.Int, value: rm }]; // Define o parâmetro @id para ser passado na query
@@ -42,3 +96,13 @@ async function getAlunoByUrgencia(urgencia) {
   const alunos = await executeQuery(query, params);
   return alunos.length > 0 ? alunos[0] : null;
 }
+
+// Exporta as funções para serem usadas nos controllers
+module.exports = {
+  getAlunoByRm,
+  getAlunoByNome,
+  getAlunoByTema,
+  getAlunoByData,
+  getAlunoByStatus,
+  getAlunoByUrgencia,
+};
