489e0c9a6e02f19747836f3cd454d44ae48ed5dc
yran336 <yranda0104@gmail.com>
2024-10-07 14:05:23 -0300

    rota

--- routes/pesquisaRoutes.js
+++ routes/pesquisaRoutes.js
@@ -5,7 +5,7 @@ const pesquisaController = require("../controllers/pesquisaController");
 router.get("/:rm", pesquisaController.getAlunoByRm);
 router.get("/:nome_completo", pesquisaController.getAlunoByNome);
 router.get("/:tema", pesquisaController.getAlunoByTema);
-router.get("/:data", pesquisaController.getAlunoByData);
+router.get("/:data_inicial/:data_final", pesquisaController.getAlunoByData);
 router.get("/:status", pesquisaController.getAlunoByStatus);
 router.get("/:urgencia", pesquisaController.getAlunoByUrgencia);
 
